a67628fe0b46a603ad2dc97d3f6dbc21cfacbaca
Lisa Feng <lisafeng10@gmail.com>
2020-07-04 17:46:10 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,8 +3,13 @@ require 'open-uri'
 require 'json'
 
 class GetRequester
+  attr_accessor :url
   
-  def initialize
+  def initialize(url)
+    @url = url
+  end
+  
+  def get_response_body
     
   end
   
